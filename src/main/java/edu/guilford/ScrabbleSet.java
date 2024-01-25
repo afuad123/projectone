2865b9a0cf8b91d51112c4fc64b0c685ee56b695
afuad727 <afuad@guilford.edu>
2024-01-24 21:43:43 -0500

    IT WORKS NOW YAYYYY

--- src/main/java/edu/guilford/ScrabbleSet.java
+++ src/main/java/edu/guilford/ScrabbleSet.java
@@ -86,59 +86,41 @@ public class ScrabbleSet {
     public ScrabbleSet() {
         tiles = new Tile[27];
         //instantiate a Tile object for each letter in the English Scrabble set
-    //     tiles[0] = new Tile(' ', 0);
-    //     tiles[1] = new Tile('A', 1);
-    //     tiles[2] = new Tile('B', 3);
-    //     tiles[3] = new Tile('C', 3);
-    //     tiles[4] = new Tile('D', 2);
-    //     tiles[5] = new Tile('E', 1);
-    //     tiles[6] = new Tile('F', 4);
-    //     tiles[7] = new Tile('G', 2);
-    //     tiles[8] = new Tile('H', 4);
-    //     tiles[9] = new Tile('I', 1);
-    //     tiles[10] = new Tile('J', 8);
-    //     tiles[11] = new Tile('K', 5);
-    //     tiles[12] = new Tile('L', 1);
-    //     tiles[13] = new Tile('M', 3);
-    //     tiles[14] = new Tile('N', 1);
-    //     tiles[15] = new Tile('O', 1);
-    //     tiles[16] = new Tile('P', 3);
-    //     tiles[17] = new Tile('Q', 10);
-    //     tiles[18] = new Tile('R', 1);
-    //     tiles[19] = new Tile('S', 1);
-    //     tiles[20] = new Tile('T', 1);
-    //     tiles[21] = new Tile('U', 1);
-    //     tiles[22] = new Tile('V', 4);
-    //     tiles[23] = new Tile('W', 4);
-    //     tiles[24] = new Tile('X', 8);
-    //     tiles[25] = new Tile('Y', 4);
-    //     tiles[26] = new Tile('Z', 10);
+        tiles[0] = new Tile(' ', 0);
+        tiles[1] = new Tile('A', 1);
+        tiles[2] = new Tile('B', 3);
+        tiles[3] = new Tile('C', 3);
+        tiles[4] = new Tile('D', 2);
+        tiles[5] = new Tile('E', 1);
+        tiles[6] = new Tile('F', 4);
+        tiles[7] = new Tile('G', 2);
+        tiles[8] = new Tile('H', 4);
+        tiles[9] = new Tile('I', 1);
+        tiles[10] = new Tile('J', 8);
+        tiles[11] = new Tile('K', 5);
+        tiles[12] = new Tile('L', 1);
+        tiles[13] = new Tile('M', 3);
+        tiles[14] = new Tile('N', 1);
+        tiles[15] = new Tile('O', 1);
+        tiles[16] = new Tile('P', 3);
+        tiles[17] = new Tile('Q', 10);
+        tiles[18] = new Tile('R', 1);
+        tiles[19] = new Tile('S', 1);
+        tiles[20] = new Tile('T', 1);
+        tiles[21] = new Tile('U', 1);
+        tiles[22] = new Tile('V', 4);
+        tiles[23] = new Tile('W', 4);
+        tiles[24] = new Tile('X', 8);
+        tiles[25] = new Tile('Y', 4);
+        tiles[26] = new Tile('Z', 10);
 
-    //    //The total number of tiles must be exactly 100.
-    //    //Each letter (and the blank) must have at least one tile associated with it.
-    //    //create a letterCount array based on the above two conditions
-    //    letterCount = new int[27];
-    //    //the number of each tile should be a random amount. the point value should be the same as a normal scrabble set
-    //      for (int i = 0; i < letterCount.length; i++) {
-    //           letterCount[i] = rand.nextInt(3) + 1;
-        //}  
-        Random random = new Random();
-        int remainingTiles = 100;
-
-        // Assign at least one tile for each letter
-        for (int i = 1; i <= 26; i++) {
-            tiles[i] = new Tile((char) ('A' + i - 1), 1);
-            remainingTiles--;
-        }
-
-        // Assign random number of tiles for each letter
-        while (remainingTiles > 0) {
-            int randomIndex = random.nextInt(26) + 1;
-            if (tiles[randomIndex] == null) {
-                tiles[randomIndex] = new Tile((char) ('A' + randomIndex - 1), 1);
-                remainingTiles--;
-            }
-        }
+      
+       letterCount = new int[27];
+       //the number of each tile should be a random amount. the point value should be the same as a normal scrabble set
+         for (int i = 0; i < letterCount.length; i++) {
+              letterCount[i] = rand.nextInt(3) + 1;
+        }  
+       
     }
 
     //METHODS
