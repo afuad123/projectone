14468737eb61610dcb5a27466161817cbcaffe32
afuad727 <afuad@guilford.edu>
2024-02-04 10:15:14 -0500

    completed everything upto "sorting and searching"

--- src/main/java/edu/guilford/ScrabbleProjectDriver.java
+++ src/main/java/edu/guilford/ScrabbleProjectDriver.java
@@ -14,94 +14,111 @@ public class ScrabbleProjectDriver {
         ScrabbleSet english = new ScrabbleSet("English");
         ScrabbleSet random = new ScrabbleSet();
 
-        //Calculate point values using the standard English ScrabbleSet object for a set of at least five test words: blue, lord, bag, shirt, hug
-        System.out.println("The point value for the word blue is " + english.getWordScore("blue"));
-        System.out.println("The point value for the word sword is " + english.getWordScore("sword"));
-        System.out.println("The point value for the word cracker is " + english.getWordScore("cracker"));
-        System.out.println("The point value for the word shirt is " + english.getWordScore("shirt"));
-        System.out.println("The point value for the word hug is " + english.getWordScore("hug"));
+        //store words in word objects
+        Word word1 = new Word("blue", english);
+        Word word2 = new Word("sword", english);
+        Word word3 = new Word("cracker", english);
+        Word word4 = new Word("shirt", english);
+        Word word5 = new Word("hug", english);
+        Word word6 = new Word("edits", english);
 
-        String filePath = "frankenstein.txt";
-        List<String> listOfWords = new ArrayList<String>(); 
+        //Calculate point values using the standard English ScrabbleSet object for a set of at least five test words, now using word.getScore()
+        System.out.println("The point value for the word blue is " + word1.getScore());
+        System.out.println("The point value for the word sword is " + word2.getScore());
+        System.out.println("The point value for the word cracker is " + word3.getScore());
+        System.out.println("The point value for the word shirt is " + word4.getScore());
+        System.out.println("The point value for the word hug is " + word5.getScore());
+        System.out.println("The point value for the word edits is " + word6.getScore());
+
+        //Compare the words using the compareTo method
+        System.out.println("The word blue compared to the word sword is " + word1.compareTo(word2));
+        System.out.println("The word sword compared to the word cracker is " + word2.compareTo(word3));
+        System.out.println("The word cracker compared to the word shirt is " + word3.compareTo(word4));
+        System.out.println("The word shirt compared to the word hug is " + word4.compareTo(word5));
+        System.out.println("The word blue compared to the word edits is " + word1.compareTo(word6));
+
+        //below is Project 1 work
+    //     String filePath = "frankenstein.txt";
+    //     List<String> listOfWords = new ArrayList<String>(); 
 
 
-        try (InputStream inputStream = ScrabbleProjectDriver.class.getResourceAsStream("/" + filePath);
-            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] words = line.split("\\s+");
-                for (String word : words) {
-                    //if (word == "dæmon"){ 
-                    //listOfWords.remove("dæmon");   
-                    //}
-                    //else {
-                    listOfWords.add(word);
-                    //}
+    //     try (InputStream inputStream = ScrabbleProjectDriver.class.getResourceAsStream("/" + filePath);
+    //         BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
+    //         String line;
+    //         while ((line = br.readLine()) != null) {
+    //             String[] words = line.split("\\s+");
+    //             for (String word : words) {
+    //                 //if (word == "dæmon"){ 
+    //                 //listOfWords.remove("dæmon");   
+    //                 //}
+    //                 //else {
+    //                 listOfWords.add(word);
+    //                 //}
                     
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        // Calculate the highest Scrabble score and the corresponding word
-        String highestScoringWord = "";
-        int highestScore = 0;
+    //             }
+    //         }
+    //     } catch (IOException e) {
+    //         e.printStackTrace();
+    //     }
+    //     // Calculate the highest Scrabble score and the corresponding word
+    //     String highestScoringWord = "";
+    //     int highestScore = 0;
         
-       // Calculate the shortest invalid Scrabble word
-       String shortestInvalidWord = "";
-       int shortestInvalidLength = 10;
+    //    // Calculate the shortest invalid Scrabble word
+    //    String shortestInvalidWord = "";
+    //    int shortestInvalidLength = 10;
 
-        for (String word : listOfWords) {
-            if (!word.contains("ê") && !word.contains("æ") && !word.contains("ô") && !word.contains("é")
-            && !word.contains("è")){
+    //     for (String word : listOfWords) {
+    //         if (!word.contains("ê") && !word.contains("æ") && !word.contains("ô") && !word.contains("é")
+    //         && !word.contains("è")){
             
-            int score = english.getWordScore(word);
-            if (score > highestScore) {
-                highestScore = score;
-                highestScoringWord = word;
-            }
-            else if (score == -1 && word.length() < shortestInvalidLength) {
-                shortestInvalidWord = word;
-                shortestInvalidLength = word.length();
-            }
-                }
-            }
+    //         int score = english.getWordScore(word);
+    //         if (score > highestScore) {
+    //             highestScore = score;
+    //             highestScoringWord = word;
+    //         }
+    //         else if (score == -1 && word.length() < shortestInvalidLength) {
+    //             shortestInvalidWord = word;
+    //             shortestInvalidLength = word.length();
+    //         }
+    //             }
+    //         }
 
-        // Print the highest scoring word
-        System.out.println("The word with the highest Scrabble score is: " + highestScoringWord);
+    //     // Print the highest scoring word
+    //     System.out.println("The word with the highest Scrabble score is: " + highestScoringWord);
 
-        // Print the shortest invalid Scrabble word
-        System.out.println("The shortest invalid Scrabble word is: " + shortestInvalidWord);
+    //     // Print the shortest invalid Scrabble word
+    //     System.out.println("The shortest invalid Scrabble word is: " + shortestInvalidWord);
 
-        //RANDOMSET
-        String hsw = "";
-        int hs = 0;
+    //     //RANDOMSET
+    //     String hsw = "";
+    //     int hs = 0;
         
-       // Calculate the shortest invalid Scrabble word
-       String siw = "";
-       int sil = 10;
+    //    // Calculate the shortest invalid Scrabble word
+    //    String siw = "";
+    //    int sil = 10;
 
-        for (String word : listOfWords) {
-            if (!word.contains("ê") && !word.contains("æ") && !word.contains("ô") && !word.contains("é")
-            && !word.contains("è")){
+    //     for (String word : listOfWords) {
+    //         if (!word.contains("ê") && !word.contains("æ") && !word.contains("ô") && !word.contains("é")
+    //         && !word.contains("è")){
             
-            int score = random.getRandomWordScore(word);
-            if (score > hs) {
-                hs = score;
-                hsw = word;
-            }
-            else if (score == -1 && word.length() < sil) {
-                siw = word;
-                sil = word.length();
-            }
-                }
-            }
+    //         int score = random.getRandomWordScore(word);
+    //         if (score > hs) {
+    //             hs = score;
+    //             hsw = word;
+    //         }
+    //         else if (score == -1 && word.length() < sil) {
+    //             siw = word;
+    //             sil = word.length();
+    //         }
+    //             }
+    //         }
 
-        // Print the highest scoring word
-        System.out.println("The word with the highest Scrabble score is: " + hsw);
+    //     // Print the highest scoring word
+    //     System.out.println("The word with the highest Scrabble score is: " + hsw);
 
-        // Print the shortest invalid Scrabble word
-        System.out.println("The shortest invalid Scrabble word is: " + siw);
+    //     // Print the shortest invalid Scrabble word
+    //     System.out.println("The shortest invalid Scrabble word is: " + siw);
 
        
    }       
--- src/main/java/edu/guilford/Word.java
+++ src/main/java/edu/guilford/Word.java
@@ -1,6 +1,8 @@
 package edu.guilford;
 
-public class Word {
+import java.util.Random;
+
+public class Word implements Comparable<Word> {
     //ATTRIBUTES
     //string that contains the word
     private String word;
@@ -14,6 +16,22 @@ public class Word {
         this.scrabbleSet = scrabbleSet;
     }
 
+    //add a constructor that generates a  String of random length 3-12 characters and with randomly determined characters
+    public Word() {
+        Random random = new Random();
+        int length = random.nextInt(10) + 3;
+        String word = "";
+        for (int i = 0; i < length; i++) {
+            char c = (char) (random.nextInt(26) + 'a');
+            word += c;
+        }
+        this.word = word;
+    }
+
+    //Include a word list (this listLinks to an external site. looks pretty comprehensive) in your project and select a random word 
+    //from the list, also ensuring that the word is at least two letters in length.
+    //is this a separate constructor?
+
     //METHODS
     //Add one method that return the Scrabble score of the word using the Scrabbleset
     public int getScore() {
@@ -25,9 +43,9 @@ public class Word {
         if (this.getScore() == otherWord.getScore()) {
             return 0;
         } else if (this.getScore() > otherWord.getScore()) {
-            return -1;
-        } else {
             return 1;
+        } else {
+            return -1;
         }
     }//If the scores are the same, but the words are not identical, then return the value of the compareTo method from String, comparing the String attributes of each Word.
     //what does this mean?
